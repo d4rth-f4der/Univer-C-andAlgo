0aa60aedff1ec6b3c9db9ce6a4b64619fdefe225
OleksandrRudniev-PD-14 <st9354750@stud.duikt.edu.ua>
2025-03-16 23:44:57 +0200

    Algorythms Lab05-task02

--- Algo Lab05-02/Algo Lab05-02.cpp
+++ Algo Lab05-02/Algo Lab05-02.cpp
@@ -2,39 +2,75 @@
 #include <vector>
 using namespace std;
 
-void removeMaxIterative(vector<int>& arr) {
-    if (arr.empty()) return;
-
-    if (arr.size() == 1) {
-        arr.pop_back();
-        return;
-    }
-
-    int maxIndex = 0;
-    for (int i = 1; i < arr.size(); ++i) {
-        if (arr[i] > arr[maxIndex]) {
-            maxIndex = i;
-        }
-    }
-
-    arr.erase(arr.begin() + maxIndex);
+void printVectorValues(const vector<int>& arr) {
+	for (int i = 0; i < arr.size(); ++i) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
 }
 
-void removeMaxRecursive(vector<int>& arr) {
-    if (arr.empty()) return;
-
-    if (arr.size() == 1) {
-        arr.pop_back();
-        return;
-    }
-
-    int maxIndex = 0;
-    for (int i = 1; i < arr.size(); ++i) {
-        if (arr[i] > arr[maxIndex]) {
-            maxIndex = i;
+int findMaxElement(const vector<int>& arr) {
+    if (arr.empty()) return -1;
+    else if (arr.size() == 1) return 0;
+    else {
+        int maxIndex = 0;
+        for (int i = 1; i < arr.size(); ++i) {
+            if (arr[i] > arr[maxIndex]) {
+                maxIndex = i;
+            }
         }
+        return maxIndex;
     }
+}
 
-    arr.erase(arr.begin() + maxIndex);
-    removeMaxRecursive(arr);
+int findMaxElementRecursive(const vector<int>& arr, int index = 0, int maxIndex = 0) {
+	if (arr.empty()) return -1;
+	else if (index == arr.size()) {
+		return maxIndex;
+	}
+	else if (arr[index] > arr[maxIndex]) {
+		return findMaxElementRecursive(arr, index + 1, index);
+	}
+	else {
+		return findMaxElementRecursive(arr, index + 1, maxIndex);
+	}
+}
+
+void removeMaxElement(vector<int>& arr) {
+	if (arr.empty()) return;
+	else if (arr.size() == 1) {
+		arr.pop_back();
+		return;
+	}
+	else {
+		int maxIndex = findMaxElement(arr);
+		arr.erase(arr.begin() + maxIndex);
+	}
+}
+
+void removeMaxElementRecursive(vector<int>& arr) {
+	if (arr.empty()) return;
+	else if (arr.size() == 1) {
+		arr.pop_back();
+		return;
+	}
+	else {
+		int maxIndex = findMaxElementRecursive(arr);
+		arr.erase(arr.begin() + maxIndex);
+	}
+}
+
+int main() {
+    vector<int> v = { 0, -5, 7, 1, 8, 9, 3, 2, 7, 1, 7 };
+	cout << "Initial vector: ";
+	printVectorValues(v);
+	cout << "Max element index: " << findMaxElement(v) << endl;
+	removeMaxElement(v);
+	cout << "Vector after removing max element: ";
+	printVectorValues(v);
+	cout << "Max element index (recursive): " << findMaxElementRecursive(v) << endl;
+	removeMaxElementRecursive(v);
+	cout << "Vector after removing max element (recursive): ";
+	printVectorValues(v);
+	return 0;
 }
